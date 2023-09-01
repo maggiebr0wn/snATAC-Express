31e1006f80c4bc1e7f231a0284ced4d5bac67b52
Maggie Brown <mfish5155@gmail.com>
2023-08-31 21:30:51 -0400

    Update cross_validation.py

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -22,12 +22,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 import sys
 
-os.chdir("/storage/home/mfisher42/scProjects/Predict_GEX/Feature_Importance_07062023")
+os.chdir("/storage/home/mfisher42/scProjects/Predict_GEX/Groups_Celltypes_Split_Pseudos_peakfilt10perc_paretofront_08302023")
 from misc_helper_functions import load_files_with_match
 
 ### 07-14-2023 ###
 # This script includes functions to perform
 # leave-one-out cross-validation
+# Addition: 08-16-2023
+# performing cross validation using training and testing
+# sets from splitting pseudobulks in half
 
 # ============================================
 def select_peaks(test, selected_peaks, gene, outdir): # get peaks from test results
@@ -47,7 +50,7 @@ def select_peaks(test, selected_peaks, gene, outdir): # get peaks from test resu
         elif "dropcol_ranker" in test_name:
             test_dir = "linreg_dropcolranker"
     # load peak list
-    peak_dir = outdir + "/" + gene + "/" + test_dir
+    peak_dir = outdir + "/" + test_dir
     match = "_" + str(npeaks) + "peaks_"
     peak_list_df = pd.read_csv(load_files_with_match(peak_dir, match)[0])
     final_peak_list = peak_list_df["Peak"].tolist()
@@ -77,6 +80,7 @@ def grid_search_init(sub_pb_peak_df, pb_gex_df, gene, test, outdir):
     model = RandomForestRegressor(random_state=0)
     model.fit(func_peaks_df, func_gex_df.values.ravel())
     # gridsearch
+    #search = GridSearchCV(estimator = model, param_grid = gs_dict, n_jobs = 20, scoring=make_scorer(r2_score))
     search = GridSearchCV(estimator = model, param_grid = gs_dict, n_jobs = 20, scoring="neg_mean_squared_error")
     search.fit(func_peaks_df, func_gex_df.values.ravel())
     # get best paramaets
@@ -84,39 +88,52 @@ def grid_search_init(sub_pb_peak_df, pb_gex_df, gene, test, outdir):
     return best_params
 
 # ============================================
-def loo_cv(test, sub_pb_peak_df, pb_gex_df, gene, outdir): # leave-one-out cross validation
+def cross_validation_fun(test, training_sub_pb_peak_df, training_pb_gex_df_sub, testing_sub_pb_peak_df, testing_pb_gex_df_sub, gene, outdir): # cross validations: LOO and pseudo-split
+    print("Running LOO cross validation")
     # select model type, LinReg or RFR:
     if "_RFR_" in test:
-        model = RandomForestRegressor(random_state = 1)
+        # perform grid search
+        print("Performing grid search")
+        best_params = grid_search_init(training_sub_pb_peak_df, training_pb_gex_df_sub, gene, test, outdir)
+        print("Grid search done")
+        model = RandomForestRegressor(random_state = 1, **best_params)
     elif "_LinReg_" in test:
         model = LinearRegression()
     # format input data; first convert to numpy arrays
-    peaks_array = sub_pb_peak_df.values.T
-    gex_array = pb_gex_df.values.T
+    training_peaks_array = training_sub_pb_peak_df.values.T
+    training_gex_array = training_pb_gex_df_sub.values.T
+    testing_peaks_array = testing_sub_pb_peak_df.values.T
+    testing_gex_array = testing_pb_gex_df_sub.values.T
     # Create new DataFrames with preserved index names
-    func_peaks_df = pd.DataFrame(peaks_array, columns=sub_pb_peak_df.index, index=sub_pb_peak_df.columns.tolist())
-    func_gex_df = pd.DataFrame(gex_array, columns=pb_gex_df.index, index=pb_peak_df.columns.tolist())
-    # initiate LOOCV to evaluate the model
-    #cv = LeaveOneOut()
-    #scores = cross_val_score(model, peaks_array, gex_array, scoring = 'neg_mean_squared_error', cv = cv, n_jobs = 10)
+    training_func_peaks_df = pd.DataFrame(training_peaks_array, columns=training_sub_pb_peak_df.index, index=training_sub_pb_peak_df.columns.tolist())
+    training_func_gex_df = pd.DataFrame(training_gex_array, columns=training_pb_gex_df_sub.index, index=training_pb_gex_df_sub.columns.tolist())
+    testing_func_peaks_df = pd.DataFrame(testing_peaks_array, columns=testing_sub_pb_peak_df.index, index=testing_sub_pb_peak_df.columns.tolist())
+    testing_func_gex_df = pd.DataFrame(testing_gex_array, columns=testing_pb_gex_df_sub.index, index=testing_pb_gex_df_sub.columns.tolist())
     # 1.0) Inititate LOO and get predicted values
     loo = LeaveOneOut()
-    y_pred_list = []
-    actual_values = []
-    for train_index, test_index in loo.split(func_peaks_df.index):
-        X_train, X_test = func_peaks_df.iloc[train_index], func_peaks_df.iloc[test_index]
-        y_train, y_test = func_gex_df.iloc[train_index], func_gex_df.iloc[test_index]
+    LOO_y_pred_list = []
+    LOO_actual_values = []
+    for train_index, test_index in loo.split(training_func_peaks_df.index):
+        X_train, X_test = training_func_peaks_df.iloc[train_index], training_func_peaks_df.iloc[test_index]
+        y_train, y_test = training_func_gex_df.iloc[train_index], training_func_gex_df.iloc[test_index]
         # fit model
         model.fit(X_train, y_train.values.ravel())
         y_pred = model.predict(X_test)
-        y_pred_list.append(y_pred[0])
-        actual_values.append(y_test.values[0][0])
-    #r2 = r2_score(actual_values, y_pred_list)
-    return y_pred_list, actual_values
+        LOO_y_pred_list.append(y_pred[0])
+        LOO_actual_values.append(y_test.values[0][0])
+    # 2.0) Initiate pseudo-split cross validation and get predicted values
+    X_train = training_func_peaks_df
+    X_test = testing_func_peaks_df
+    y_train = training_func_gex_df
+    y_test = testing_func_gex_df
+    model.fit(X_train, y_train.values.ravel())
+    SPLIT_y_pred = model.predict(X_test).flatten().tolist()
+    SPLIT_actual_values = y_test.values.flatten().tolist()
+    return LOO_y_pred_list, LOO_actual_values, SPLIT_y_pred, SPLIT_actual_values
 
 # ============================================
-def make_loo_plot(predicted, actual, filename):
-    fig, ax = plt.subplots()
+def make_cv_plot(predicted, actual, filename):
+    fig, ax = plt.subplots()    
     plt.scatter(actual, predicted)
     plt.xlabel("Actual GEX")
     plt.ylabel("Predicted GEX")
@@ -130,3 +147,9 @@ def make_loo_plot(predicted, actual, filename):
     r2_text = f'R2 = {r2:.2f}'
     ax.text(0.05, 0.95, r2_text, transform=ax.transAxes, ha='left', va='top')
     plt.savefig(filename, format='pdf')
+    return r2
+
+# ============================================
+def split_cross_val(test, training_pb_peak_df, training_gex_peak_df, testing_pb_peak_df, testing_gex_peak_df, gene, outdir):
+    print("Running pseudobulk split cross validation")
+    
