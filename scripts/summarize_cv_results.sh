b36354d080d9fe18d73fc65c5f9fa0f975571810
Maggie Brown <mfish5155@gmail.com>
2024-02-14 15:41:04 -0500

    Update summarize_cv_results.sh
    updates per HPC version

--- scripts/summarize_cv_results.sh
+++ scripts/summarize_cv_results.sh
@@ -2,7 +2,6 @@
 
 # 10-25-2023
 # This script goes through the results and summarizes the cross validation results
-# for models using all peaks or 95% selected peaks
 #
 # How to run:
 # ./summarize_cv_results.sh -d <results_dir>
@@ -21,16 +20,11 @@ echo -e "Gene\tMethod\tnPeaks\tPeakCat\tCV_R2" > cv_summary.txt
 for file in $result_files
 do
         gene="$(echo "$file" | rev | cut -d"/" -f1 | rev | cut -d"_" -f1)"
-        #echo $gene
         method="$(echo "$file" | rev | cut -d"/" -f1 | rev | cut -d"_" -f2,3,4)"
-        #echo $method
         # get info for all peaks
         npeaks_all="$(awk -F"," '{if (NR == 2) {print $1}}' "$file")"
         r2_all="$(awk -F"," '{if (NR == 2) {print $2}}' "$file")"
         # get info for select peaks
-        npeaks_select="$(awk -F"," '{if (NR == 3) {print $1}}' "$file")"
-        r2_select="$(awk -F"," '{if (NR == 3) {print $2}}' "$file")"
         # save to summary
         echo -e $gene"\t"$method"\t"$npeaks_all"\tAll_Peaks\t"$r2_all >> cv_summary.txt
-        echo -e $gene"\t"$method"\t"$npeaks_select"\tSelect_Peaks\t"$r2_select >> cv_summary.txt
 done
