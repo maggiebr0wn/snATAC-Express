ddfd164c055467835e5bcf3b76a0cf6d8c602373
maggiebrown <mfish5155@gmail.com>
2025-06-12 20:19:04 -0700

    add exmaple data

--- snatac_express/_version.py
+++ snatac_express/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev89+g3e1fc1e.d20250613'
-__version_tuple__ = version_tuple = (0, 1, 'dev89', 'g3e1fc1e.d20250613')
+__version__ = version = '0.1.dev92+gf23bd26.d20250613'
+__version_tuple__ = version_tuple = (0, 1, 'dev92', 'gf23bd26.d20250613')
